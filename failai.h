722b90a0cdefba7be82c127c0cba5baf48846b68
Emilija Žižytė <zizyte.emilija@gmail.com>
2025-10-09 23:08:11 +0300

    failai.h
    Failas deklaruoja funkcijas, kad jas galėtumėte naudoti kituose failuose

---
+++ failai.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "studentas.h"
+#include <fstream>
+#include <chrono>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+
+void generuokFailus();
+void analizuokVisusFailusMinimaliai();
