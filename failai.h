73e4b417a5b02576de8b09365cade184efef9a6b
Emilija Žižytė <zizyte.emilija@gmail.com>
2025-11-06 17:09:48 +0200

    galutinis

--- failai.h
+++ failai.h
@@ -11,11 +11,33 @@
 #include <algorithm>
 #include <functional>
 
+struct Rezultatai {
+    double readTime = 0;   
+    double splitTime = 0;  
+    double writeTime = 0;  
+    double memMB = 0;      
+
+   
+    double readTimeVector = 0;
+    double splitTimeVector = 0;
+    double writeTimeVector = 0;
+    double memMBVector = 0;
+
+    
+    double readTimeList = 0;
+    double splitTimeList = 0;
+    double writeTimeList = 0;
+    double memMBList = 0;
+};
+
+
 void generuokFailus();
 void analizuokVisusFailusMinimaliai();
 void testuokStrategijas();
-void strategija1_skaidymas();
-void Strategija2();
-void strategija1_STL_vectoriui();
-void Strategija2_STL_vectoriui();
-void strategija3();
+
+
+Rezultatai strategija1_skaidymas(const std::string& failas);
+Rezultatai Strategija2(const std::string& failas);
+Rezultatai strategija1_STL_vectoriui(const std::string& failas);
+void Strategija3(const std::string& failas);
+Rezultatai strategija2_STL_vectoriui(const std::string& failas);
