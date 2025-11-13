07b6a682ca72a5ea40a9d3c3e8894897bacb6f3b
Emilija Žižytė <zizyte.emilija@gmail.com>
2025-11-13 21:38:22 +0200

    ger

--- pagrindinis.cpp
+++ pagrindinis.cpp
@@ -2,12 +2,26 @@
 #include <vector>
 #include <list>
 #include <ctime>
-#include <sstream> 
 #include "studentas.h"
 #include "failai.h"
 
+// ======== Template funkcija rezultatams rodyti ========
+template <typename T>
+void rodytiRezultatus(const T& container) {
+    std::cout << "\n=== Studentu rezultatai ===\n";
+    for (const auto& s : container) {
+        std::cout << "Vardas: " << s.getVard()
+            << ", Pavarde: " << s.getPav()
+            << ", Egzaminas: " << s.getEgzas()
+            << ", Vidurkis: " << s.getRezVid()
+            << ", Mediana: " << s.getRezMed() << "\n";
+    }
+}
+
+// ======== Main funkcija ========
 int main() {
     std::srand(static_cast<unsigned>(std::time(0)));
+
     std::vector<Studentas> GrupeVector;
     std::list<Studentas> GrupeList;
 
@@ -19,50 +33,82 @@ int main() {
         std::cout << "4 - Generuoti testinius failus\n";
         std::cout << "5 - Nuskaityti visus failus\n";
         std::cout << "6 - Testuoti strategijas\n";
+        std::cout << "7 - 1mln 10mln vektoriui testavimas\n";
         std::cout << "Jusu pasirinkimas: ";
+
         int pasirinkimas;
         std::cin >> pasirinkimas;
+
         if (std::cin.fail()) {
             std::cin.clear();
             std::cin.ignore(10000, '\n');
-            std::cout << "Bandykite dar karta.\n";
+            std::cout << "Bloga ivestis. Bandykite dar karta.\n";
             continue;
         }
+
         std::cin.ignore(10000, '\n');
 
         if (pasirinkimas == 1) {
+            std::cout << "\n=== Ivedimas ir issaugojimas ===\n";
             Studentas s = ivesk();
+
             GrupeVector.push_back(s);
-            std::cout << "Studentas '" << s.getVard() << " " << s.getPav() << "' issaugotas vectoriuje.\n";
+            std::cout << "Studentas '" << s.getVard() << " " << s.getPav()
+                << "' issaugotas vectoriuje adresu: " << &GrupeVector.back() << "\n";
+
             GrupeList.push_back(s);
-            std::cout << "Studentas '" << s.getVard() << " " << s.getPav() << "' issaugotas liste.\n";
+            std::cout << "Studentas '" << s.getVard() << " " << s.getPav()
+                << "' issaugotas liste adresu: " << &GrupeList.back() << "\n";
         }
         else if (pasirinkimas == 2) {
-            if (GrupeVector.empty()) { std::cout << "Sarasas tuscias.\n"; continue; }
-            std::cout << std::left << std::setw(15) << "Vardas" << std::left << std::setw(15) << "Pavarde"
-                << std::right << std::setw(15) << "Galutinis(Vid.)" << std::right << std::setw(15) << "Galutinis(Med.)" << "\n";
-            for (const auto& s : GrupeVector) {
-                std::cout << std::left << std::setw(15) << s.getVard() << std::left << std::setw(15) << s.getPav()
-                    << std::right << std::setw(15) << std::fixed << std::setprecision(2) << s.getRezVid()
-                    << std::right << std::setw(15) << std::fixed << std::setprecision(2) << s.getRezMed() << "\n";
-            }
+            rodytiRezultatus(GrupeVector);
+            rodytiRezultatus(GrupeList);
         }
-        else if (pasirinkimas == 3) break;
-        else if (pasirinkimas == 4) generuokFailus();
-        else if (pasirinkimas == 5) analizuokVisusFailusMinimaliai();
+        else if (pasirinkimas == 3) {
+            std::cout << "Programa baigta.\n";
+            break;
+        }
+        else if (pasirinkimas == 4) {
+            generuokFailus();
+        }
+        else if (pasirinkimas == 5) {
+            analizuokVisusFailusMinimaliai();
+        }
         else if (pasirinkimas == 6) {
-            std::string failas;
             int strategija;
+            std::string failas;
             std::cout << "Iveskite failo pavadinima: ";
             std::cin >> failas;
-            std::cout << "Kurios strategijos noretumet (1-3): ";
+
+            std::cout << "Kurios strategijos noretumet: \n"
+                << "1 - Skaidymas i du konteinerius\n"
+                << "2 - Vieno naujo konteinerio naudojimas\n"
+                << "3 - Greiciausios strategijos nustatymas ir STL vector pritaikymas\n";
             std::cin >> strategija;
-            if (strategija == 1) strategija1_skaidymas(failas);
-            else if (strategija == 2) Strategija2(failas);
-            else if (strategija == 3) Strategija3(failas);
-            else std::cout << "Netinkamas pasirinkimas.\n";
+
+            if (strategija == 1) {
+                strategija1_skaidymas(failas);
+            }
+            else if (strategija == 2) {
+                Strategija2(failas);
+            }
+            else if (strategija == 3) {
+                Strategija3(failas);
+            }
+            else {
+                std::cout << "Netinkamas pasirinkimas. Bandykite dar karta.\n";
+            }
         }
-        else std::cout << "Netinkamas pasirinkimas.\n";
+        else if (pasirinkimas == 7) {
+            std::string failas;
+            std::cout << "Iveskite failo pavadinima: ";
+            std::cin >> failas;
+            strategija1_STL_vectoriui(failas);
+        }
+        else {
+            std::cout << "Netinkamas pasirinkimas. Bandykite dar karta.\n";
+        }
     }
+
     return 0;
 }
